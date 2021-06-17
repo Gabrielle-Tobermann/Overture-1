62872d9cc1da6cf9d3593309930bbd742ac0fd55
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-17 16:20:42 -0500

    setup order view, created cards, orders are deleting properly

---
+++ src/components/OrderCard.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+  Card,
+  Button,
+  Popover,
+  PopoverHeader,
+  PopoverBody
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import { ButtonContainer, Ulist } from '../styles/ItemsStyle';
+import { deleteOrder } from '../helpers/data/ordersData';
+
+function OrderCard({
+  fullName,
+  firebaseKey,
+  email,
+  insurance,
+  date,
+  userID,
+  setOrders
+}) {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+  const handleButtonClick = () => {
+    deleteOrder(firebaseKey).then((resp) => setOrders(resp));
+  };
+
+  return (
+    <div style={{ width: 'min-content', margin: '2%' }}>
+      <Card body style={{ width: 'fit-content', border: 'solid 1px black' }}>
+        <button className='cardButton' id={firebaseKey}>{fullName}</button>
+        <Popover
+          placement="right"
+          isOpen={popoverOpen}
+          target={firebaseKey}
+          toggle={toggle}
+          >
+          <PopoverHeader>{fullName}</PopoverHeader>
+          <PopoverBody>
+            <Ulist>
+              <li>{email}</li>
+              <li>{insurance}</li>
+              <li>{date}</li>
+              <li>Processed by {userID}</li>
+              <li>Items do a forEach with price</li>
+            </Ulist>
+            <ButtonContainer>
+              <Button color="dark" className="rounded-pill" onClick={handleButtonClick}>Delete</Button>
+            </ButtonContainer>
+          </PopoverBody>
+        </Popover>
+      </Card>
+    </div>
+  );
+}
+
+OrderCard.propTypes = {
+  fullName: PropTypes.string,
+  firebaseKey: PropTypes.string,
+  email: PropTypes.string,
+  insurance: PropTypes.string,
+  date: PropTypes.string,
+  userID: PropTypes.string,
+  setOrders: PropTypes.func
+};
+
+export default OrderCard;
--- src/views/OrderView.js
+++ src/views/OrderView.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
+import OrderCard from '../components/OrderCard';
 import OrderForm from '../components/OrderForm';
+import { getOrders } from '../helpers/data/ordersData';
+import {
+  ButtonSearchContainer, SearchInput, TopContainer, ViewTitle
+} from '../styles/ItemsStyle';
 
-export default function OrderView() {
+function OrderView() {
+  const [orders, setOrders] = useState([]);
+  const [adding, setAdding] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    getOrders().then((resp) => setOrders(resp));
+  }, []);
+
+  const handleButtonClick = () => {
+    setAdding(true);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div>
-      <h1>Orders</h1>
-      <OrderForm/>
+      <TopContainer>
+        <ViewTitle>Orders</ViewTitle>
+        <ButtonSearchContainer>
+        <Button color="dark" style={{ fontSize: '20px', fontWeight: 'bold' }} onClick={handleButtonClick}>+</Button>
+        <SearchInput placeholder='Search' value={searchValue} onChange={handleSearchChange}></SearchInput>
+      </ButtonSearchContainer>
+      </TopContainer>
+      {adding && <OrderForm
+      setItems={setOrders}
+      />}
+        { searchValue === ''
+          ? ''
+          : orders.filter((order) => order.fullName === 'instrument' && order.fulName.toLowerCase().includes(searchValue.toLowerCase())).map((item) => (
+          <OrderCard
+            key={item.firebaseKey}
+            fullName={item.fullName}
+            email={item.email}
+            firebaseKey={item.firebaseKey}
+            setOrders={setOrders}
+            date={item.date}
+            />
+          ))
+        }
+        {
+          orders.map((order) => (
+            <OrderCard
+            key={order.firebaseKey}
+            fullName={order.fullName}
+            email={order.email}
+            firebaseKey={order.firebaseKey}
+            setOrders={setOrders}
+            date={order.date}
+            />
+          ))
+        }
     </div>
   );
 }
+
+export default OrderView;
