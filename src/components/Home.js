d20df640c6fb4556abe983da10573cabd4cad0f6
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-18 21:48:24 -0500

    styled home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {
+  WholeContainer, H2Container, H2, H3, H1
+} from '../styles/HomeStyle';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <WholeContainer>
+      <H1>O v e r t u r e</H1>
+      <H2Container>
+        <H2>Keep track of your inventory.</H2>
+        <H2>Reference your orders.</H2>
+        <H2>Visualize your revenue.</H2>
+      </H2Container>
+      <H3>All in one place.</H3>
+    </WholeContainer>
   );
 }
