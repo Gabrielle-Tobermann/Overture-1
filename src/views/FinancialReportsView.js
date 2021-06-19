bc95988ac8c80d15010c0488431090a0084eadb1
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-19 13:09:02 -0500

    working on chart

--- src/views/FinancialReportsView.js
+++ src/views/FinancialReportsView.js
@@ -1,6 +1,20 @@
 import React from 'react';
+// import { Bar } from 'react-chartjs-2';
 
 export default function FinancialReportsView() {
+  // const [chartData, setChartData] = useState({});
+
+  // const chart = () => {
+  //   setChartData({
+  //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  //     datasets: [
+  //       {
+  //         label: 'Revenue',
+  //         data: [100, 320, 450, 130, 500, 600, 300, 350, 240, 450, 450, 200]
+  //       }
+  //     ]
+  //   });
+  // };
   return (
     <div>
       <h1>Financial Reports</h1>
