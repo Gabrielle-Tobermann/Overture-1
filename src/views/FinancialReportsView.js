d4d90e07eb87849e6fc73f72a530cf13387bed72
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-20 17:43:00 -0500

    working through passing jan, feb and march values to chart

--- src/views/FinancialReportsView.js
+++ src/views/FinancialReportsView.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { getOrderItems } from '../helpers/data/ordersData';
 
 function FinancialReportsView({ orders, items }) {
+  const [orderItems, setOrderItems] = useState([]);
   const [chartData, setChartData] = useState({
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     datasets: [
@@ -16,49 +17,96 @@ function FinancialReportsView({ orders, items }) {
     ]
   });
 
-  const [orderItems, setOrderItems] = useState([]);
+  console.warn(setChartData);
 
   useEffect(() => {
     getOrderItems().then((resp) => setOrderItems(resp));
   }, []);
 
-  console.warn(items);
-
   const janItems = [];
   const janOrderItems = [];
   const janOrders = [];
+  let janFinancials = 0;
+  const febItems = [];
+  const febOrderItems = [];
+  let febFinancials = 0;
+  const febOrders = [];
+  const marchItems = [];
+  const marchOrderItems = [];
+  const marchOrders = [];
+  let marchFinancials = 0;
 
-  orders.map((order) => {
-    if (order.date.split('-')[1] === '01') {
-      janOrders.push(order);
-      console.warn('janOrders', janOrders);
+  orders.forEach((order) => {
+    switch (order.date.split('-')[1]) {
+      case order.date.split('-')[1] === '01':
+        janOrders.push(order);
+        orderItems.forEach((orderItem) => {
+          janOrders.forEach((janOrder) => {
+            if (orderItem.transactionID === janOrder.transactionID) {
+              janOrderItems.push(orderItem);
+            }
+          });
+        });
+        items.forEach((item) => {
+          janOrderItems.forEach((janOrderItem) => {
+            if (janOrderItem.itemID === item.itemID) {
+              janItems.push(item.price);
+            }
+            janFinancials = janItems.reduce((a, b) => a + b, 0);
+          });
+        });
+        break;
+      case order.date.split('-')[1] === '02':
+        febOrders.push(order);
+        orderItems.forEach((orderItem) => {
+          febOrders.forEach((febOrder) => {
+            if (orderItem.transactionID === febOrder.transactionID) {
+              febOrderItems.push(orderItem);
+            }
+          });
+        });
+        items.forEach((item) => {
+          febOrderItems.forEach((febOrderItem) => {
+            if (febOrderItem.itemID === item.itemID) {
+              febItems.push(item.price);
+            }
+          });
+        });
+        break;
+      case order.date.split('-')[1] === '03':
+        marchOrders.push(order);
+        orderItems.forEach((orderItem) => {
+          marchOrders.forEach((marchOrder) => {
+            if (orderItem.transactionID === marchOrder.transactionID) {
+              marchOrderItems.push(orderItem);
+            }
+          });
+        });
+        items.forEach((item) => {
+          marchOrderItems.forEach((marchOrderItem) => {
+            if (marchOrderItem.itemID === item.itemID) {
+              marchItems.push(item.price);
+            }
+          });
+        });
+        break;
+      default: console.warn('no items match');
     }
-    return janOrders;
   });
 
-  orderItems.map((orderItem) => {
-    janOrders.forEach((janOrder) => {
-      if (janOrder.transactionID === orderItem.transactionID) {
-        janOrderItems.push(orderItem);
-      }
-      console.warn(janOrderItems);
-      return janOrderItems;
+  useEffect(() => {
+    setChartData({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Revenue',
+          data: [100, 320, 450, 130, 500, 600, 300, 350, 240, 450, 450, 200],
+          backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+          borderWidth: 4
+        }
+      ]
     });
-    return janOrderItems;
-  });
-
-  items.map((item) => {
-    janOrderItems.forEach((janItem) => {
-      if (janItem.itemID === item.itemID) {
-        janItems.push(item.price);
-      }
-      return janItems;
-    });
-    console.warn('jandItems', janItems);
-    return janItems;
-  });
-
-  console.warn(setChartData);
+  }, []);
 
   return (
     <div>
