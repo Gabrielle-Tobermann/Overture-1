2d2b9d79df7c34a852305044786927cad2f2cbac
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-20 18:44:03 -0500

    chart working for jan feb march

--- src/views/FinancialReportsView.js
+++ src/views/FinancialReportsView.js
@@ -35,11 +35,48 @@ function FinancialReportsView({ orders, items }) {
   const marchOrderItems = [];
   const marchOrders = [];
   let marchFinancials = 0;
+  const apItems = [];
+  const apOrderItems = [];
+  const apOrders = [];
+  let apFinancials = 0;
+  const mayItems = [];
+  const mayOrderItems = [];
+  const mayOrders = [];
+  let mayFinancials = 0;
+  const juneItems = [];
+  const juneOrderItems = [];
+  const juneOrders = [];
+  let juneFinancials = 0;
+  const julyItems = [];
+  const julyOrderItems = [];
+  const julyOrders = [];
+  let julyFinancials = 0;
+  const augItems = [];
+  const augOrderItems = [];
+  const augOrders = [];
+  let augFinancials = 0;
+  const sepItems = [];
+  const sepOrderItems = [];
+  const sepOrders = [];
+  let sepFinancials = 0;
+  const octItems = [];
+  const octOrderItems = [];
+  const octOrders = [];
+  let octFinancials = 0;
+  const novItems = [];
+  const novOrderItems = [];
+  const novOrders = [];
+  let novFinancials = 0;
+  const decItems = [];
+  const decOrderItems = [];
+  const decOrders = [];
+  let decFinancials = 0;
 
   orders.forEach((order) => {
-    switch (order.date.split('-')[1]) {
+    switch (true) {
       case order.date.split('-')[1] === '01':
         janOrders.push(order);
+        console.warn(janOrders);
         orderItems.forEach((orderItem) => {
           janOrders.forEach((janOrder) => {
             if (orderItem.transactionID === janOrder.transactionID) {
@@ -52,9 +89,10 @@ function FinancialReportsView({ orders, items }) {
             if (janOrderItem.itemID === item.itemID) {
               janItems.push(item.price);
             }
-            janFinancials = janItems.reduce((a, b) => a + b, 0);
           });
         });
+        janFinancials = janItems.reduce((a, b) => Number(a) + Number(b), 0);
+        console.warn('janFinancials', janFinancials);
         break;
       case order.date.split('-')[1] === '02':
         febOrders.push(order);
@@ -72,6 +110,7 @@ function FinancialReportsView({ orders, items }) {
             }
           });
         });
+        febFinancials = febItems.reduce((a, b) => a + b, 0);
         break;
       case order.date.split('-')[1] === '03':
         marchOrders.push(order);
@@ -89,6 +128,43 @@ function FinancialReportsView({ orders, items }) {
             }
           });
         });
+        marchFinancials = marchItems.reduce((a, b) => a + b, 0);
+        break;
+      case order.date.split('-')[1] === '04':
+        apOrders.push(order);
+        orderItems.forEach((orderItem) => {
+          apOrders.forEach((apOrder) => {
+            if (orderItem.transactionID === apOrder.transactionID) {
+              apOrderItems.push(orderItem);
+            }
+          });
+        });
+        items.forEach((item) => {
+          apOrderItems.forEach((apOrderItem) => {
+            if (apOrderItem.itemID === item.itemID) {
+              apItems.push(item.price);
+            }
+          });
+        });
+        apFinancials = marchItems.reduce((a, b) => a + b, 0);
+        break;
+      case order.date.split('-')[1] === '05':
+        mayOrders.push(order);
+        orderItems.forEach((orderItem) => {
+          mayOrders.forEach((mayOrder) => {
+            if (orderItem.transactionID === mayOrder.transactionID) {
+              mayOrderItems.push(orderItem);
+            }
+          });
+        });
+        items.forEach((item) => {
+          mayOrderItems.forEach((mayOrderItem) => {
+            if (mayOrderItem.itemID === item.itemID) {
+              mayItems.push(item.price);
+            }
+          });
+        });
+        mayFinancials = marchItems.reduce((a, b) => a + b, 0);
         break;
       default: console.warn('no items match');
     }
@@ -100,7 +176,7 @@ function FinancialReportsView({ orders, items }) {
       datasets: [
         {
           label: 'Revenue',
-          data: [100, 320, 450, 130, 500, 600, 300, 350, 240, 450, 450, 200],
+          data: [Number(janFinancials), febFinancials, marchFinancials, 130, 500, 600, 300, 350, 240, 450, 450, 200],
           backgroundColor: ['rgba(75, 192, 192, 0.6)'],
           borderWidth: 4
         }
